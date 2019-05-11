aa7a62d1e72c617239f2a024550649376dd39195
wilson javier <javierre10va@gmail.com>
2019-05-11 18:37:17 -0500

    * edit variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_URL: 'https://superfud-back.herokuapp.com/',
-  CLIENT_SECRET: '',
-  CLIENT_ID: 1
+  CLIENT_SECRET: '3XvK1E5BGMJgFeroxY5AiZHs30CTkN6t0n614frB',
+  CLIENT_ID: 2
 };
