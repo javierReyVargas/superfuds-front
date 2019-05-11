d117ee1a029607044642271300ffac1924018b20
wilson javier <javierre10va@gmail.com>
2019-05-11 17:51:28 -0500

    * added express.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express'), path = require('path');
+
+
+const app = express();
+
+app.use( express.static('./dist/superfud-front') );
+
+app.get( '/*', ( req, res) => {
+
+  res.sendFile( path.join( __dirname, '/dist/superfud-front/index.html'));
+
+});
+
+
+app.listen( process.env.PORT || 8080, () => {
+  console.log('Server started');
+});
